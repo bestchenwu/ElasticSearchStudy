9186f88c88791741abc86cab2bc2e420f5a92434
chenwu <chenwu@meitunmama.com>
2020-09-14 18:06:07 +0800

    提交esClient

--- src/main/java/common/ElasticSearchClient.java
+++ src/main/java/common/ElasticSearchClient.java
@@ -2,29 +2,35 @@ package common;
 
 import common.model.Pair;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
-import java.util.stream.StreamSupport;
 
 class ElasticSearchClient {
 
     private Pair<String,Integer>[] hostsAndPorts;
 
-    private ElasticSearchClient(){
-
+    private ElasticSearchClient() throws IOException{
+        loadFromProperties();
     }
 
-    private String[] loadFromProperties() throws IOException {
+    private void loadFromProperties() throws IOException {
         Properties properties = new Properties();
         InputStream is = getClass().getResourceAsStream("elasticSearchServer.properties");
         properties.load(is);
         String serverIpPorts = properties.getProperty(ElasticSearchConstants.SERVER_IP_PORTS);
         String[] splitArray = serverIpPorts.split(SymbolConstants.SYMBOL_DH);
         int size = splitArray.length;
-        hostsAndPorts = new Pair[size];
-        Arrays.stream(splitArray).forEach();
+        List<Pair> hostsAndPortList = new ArrayList<Pair>();
+        int index = 0;
+        Arrays.stream(splitArray).forEach(item->{
+            String[] ipPortArray = item.split(SymbolConstants.SYMBOL_MH);
+            Pair<String,Integer> ipPort = new Pair<>(ipPortArray[0],Integer.parseInt(ipPortArray[1]));
+            hostsAndPortList.add(ipPort);
+        });
+        hostsAndPortList.toArray(hostsAndPorts);
     }
 }
